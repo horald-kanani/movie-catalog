8e3ed66da70a55e68eb728bf270346dca6ac03b8
Horald Kanani <fr0650@wayne.edu>
2022-04-16 21:37:39 -0400

    Create movie model

---
+++ Models/movie.ts
@@ -0,0 +1,11 @@
+export interface Movie {
+  movieID: number
+  movieName:string
+  thumbnailURL: string
+  largeImgURL: string
+  releaseDate: number
+  tomatoScore: string
+  mpaaRating: string
+  description:string
+
+}
