e7903d1014118a5d492ca8685a36b545c5d013d8
Horald Kanani <fr0650@wayne.edu>
2022-04-22 20:26:20 -0400

    Create movie-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MovieCardComponent
   ],
   imports: [
     BrowserModule,
