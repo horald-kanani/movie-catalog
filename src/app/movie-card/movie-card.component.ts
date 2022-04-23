e341e09abefb9b7241e74fa8a40c6fc61ff030a2
Horald Kanani <fr0650@wayne.edu>
2022-04-22 20:39:15 -0400

    Finish movie-catalog and change to scss

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { Movie } from '../model/movie';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.css']
+  styleUrls: ['./movie-card.component.scss']
 })
 export class MovieCardComponent implements OnInit {
+
+  @Input()
+  movie!: Movie;
+  @Output()
+  movieSelected = new EventEmitter<Movie>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+ 
+onClick(){
+  this.movieSelected.emit(this.movie);
+}
 }
