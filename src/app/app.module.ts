eb5b7cb38e4bfff047a20c913b35ced199fb74f1
Horald Kanani <fr0650@wayne.edu>
2022-04-22 21:00:07 -0400

    Add service and modify other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { SearchFilterPipe } from './pipes/search-filter.pipe';
+import { SortPipe } from './pipes/sort.pipe';
+import { TomatoCardComponent } from './tomato-card/tomato-card.component';
+import { HighlightedDirective } from './directives/highlighted.directive';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieService } from './services/movie.service';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    MovieCardComponent,
     HomeComponent,
-    MovieCardComponent
+    SearchFilterPipe,
+    SortPipe,
+    TomatoCardComponent,
+    HighlightedDirective,
+    MovieDetailsComponent
+ 
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    MovieService
+   
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/movie-details/movie-details.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {  Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Movie } from '../model/movie';
+import { MovieService } from '../services/movie.service';
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.component.html',
+  styleUrls: ['./movie-details.component.scss']
+})
+export class MovieDetailsComponent implements OnInit {
+  movie$!: Observable<Movie>;
+  movie!: Movie;
+  constructor( private router: Router,private svc:MovieService) {
+    
+  }
+
+  ngOnInit(): void {
+    console.log(this.svc.selectedMovie);
+    //this.movie$ = this.svc.selectedMovie;
+    const movieId = this.router.url.split("/")[2];
+    this.movie$ = this.svc.getMovieById(movieId);
+    this.movie$.pipe(
+      tap((movie)=>{
+        console.log(movie);
+        this.movie =movie;
+
+      }
+      )
+    ).subscribe();
+  
+  }
+
+}
